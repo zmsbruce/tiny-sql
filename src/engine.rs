bab9b7709e3a86843904292b6570bb8c4c722241
zmsbruce <zmsbruce@163.com>
2025-03-01 12:56:32 +0800

    chore: :bulb: 添加 engine 相关的注释

--- src/engine.rs
+++ src/engine.rs
@@ -7,17 +7,20 @@ use crate::{
     Result,
 };
 
+/// 数据库引擎，负责管理事务，执行事务操作
 pub struct Engine<S: Storage> {
     mvcc: Mvcc<S>,
 }
 
 impl<S: Storage> Engine<S> {
+    /// 创建一个新的数据库引擎
     pub fn new(storage: S) -> Self {
         Self {
             mvcc: Mvcc::new(storage),
         }
     }
 
+    /// 开启一个新的事务
     pub fn start_txn(&mut self) -> Result<Transaction<S>> {
         Ok(Transaction {
             txn: self.mvcc.start_txn()?,
@@ -25,23 +28,36 @@ impl<S: Storage> Engine<S> {
     }
 }
 
+/// 数据库引擎内部的键
+///
+/// - `Table(String)`：标识存储表信息
+/// - `Row(String, Value)`：标识存储行数据
 #[derive(Debug, Serialize, Deserialize)]
 enum Key {
     Table(String),
     Row(String, Value),
 }
 
+/// 数据库引擎内部的键前缀
+///
+/// - `Table`：标识表信息的前缀
+/// - `Row(String)`：标识行数据的前缀
+///
+/// 注：和 `storage::mvcc::MvccKey` 不同，虽然前缀中也使用了字符串，但字符串长度和 Key 中的 `String` 长度相同，
+/// 因此不需要删除前缀中的长度信息，直接使用 bincode 序列化即可。
 #[derive(Debug, Serialize, Deserialize)]
 enum KeyPrefix {
     Table,
     Row(String),
 }
 
+/// 数据库事务，对 `MvccTxn` 进行了封装，提供了更高级别的操作
 pub struct Transaction<S: Storage> {
     txn: MvccTxn<S>,
 }
 
 impl<S: Storage> Transaction<S> {
+    /// 获取表信息
     pub fn get_table_info(&self, table_name: &str) -> Result<Option<Table>> {
         let key = Key::Table(table_name.to_string());
         let table = self
@@ -52,11 +68,14 @@ impl<S: Storage> Transaction<S> {
         Ok(table)
     }
 
+    /// 创建行数据
     pub fn create_row(&self, table_name: &str, row: &Row) -> Result<()> {
+        // 如果表不存在，返回错误
         let table = self
             .get_table_info(table_name)?
             .ok_or(InternalError(format!("Table {table_name} not found")))?;
 
+        // 检查行数据是否符合表定义
         for (column, row) in table.columns.iter().zip(row.iter()) {
             match row.data_type() {
                 None if !column.nullable => {
@@ -83,7 +102,9 @@ impl<S: Storage> Transaction<S> {
         Ok(())
     }
 
+    /// 创建表
     pub fn create_table(&self, table: Table) -> Result<()> {
+        // 检查表是否已经存在，如果存在则返回错误
         if self.get_table_info(&table.name)?.is_some() {
             return Err(InternalError(format!(
                 "Table {} already exists",
@@ -91,6 +112,7 @@ impl<S: Storage> Transaction<S> {
             )));
         }
 
+        // 检查表是否有列定义，如果没有则返回错误
         if table.columns.is_empty() {
             return Err(InternalError(format!(
                 "Table {} has no columns",
@@ -105,6 +127,7 @@ impl<S: Storage> Transaction<S> {
         Ok(())
     }
 
+    /// 扫描表
     pub fn scan_table(&self, table_name: &str) -> Result<Vec<Row>> {
         let prefix = KeyPrefix::Row(table_name.to_string());
         let result = self.txn.scan_prefix(&bincode::serialize(&prefix)?)?;
@@ -118,11 +141,13 @@ impl<S: Storage> Transaction<S> {
         Ok(rows)
     }
 
+    /// 提交事务
     #[inline]
     pub fn commit(&self) -> Result<()> {
         self.txn.commit()
     }
 
+    /// 回滚事务
     #[inline]
     pub fn rollback(&self) -> Result<()> {
         self.txn.rollback()
